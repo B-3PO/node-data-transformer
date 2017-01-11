067b7c92a7bcf8bee3462de86357a81ba1ac4999
Ben Rubin <benrubin@Bens-MacBook-Pro-3.local>
2017-01-10 21:13:17 -0600

    rewrite query builder and refactor struct to make more than 2 layers building correct queries

--- tests/test.js
+++ tests/test.js
@@ -8,7 +8,40 @@ transformer.addDatabase({
 });
 
 
-// --- transformers ---
+// --- resources ---
+
+transformer.defineResource('locations', {
+  table: 'locations',
+  fields: {
+    id: {dataType: transformer.ID},
+    name: {dataTpe: transformer.STRING},
+    description: {dataTpe: transformer.STRING}
+  },
+
+  relationships: [
+    'location_menus.location_id'
+  ]
+});
+
+transformer.defineResource('location_menus', {
+  table: 'location_menus',
+  fields: {
+    id: {dataType: transformer.ID},
+    location_id: {dataType: transformer.INT},
+    menu_id: {dataType: transformer.INT}
+  },
+
+  relationships: [
+    {
+      resource: 'locations.id',
+      field: 'location_id' // this is assum,ed to be id if not defined
+    },
+    {
+      resource: 'menus.id',
+      field: 'menu_id' // this is assum,ed to be id if not defined
+    }
+  ]
+});
 
 transformer.defineResource('menus', {
   table: 'menus',
@@ -18,7 +51,8 @@ transformer.defineResource('menus', {
   },
 
   relationships: [
-    'menu_items.menu_id'
+    'menu_items.menu_id',
+    'location_menus.menu_id' // TODO `findJoinRelation()`: coimment this out to break
   ]
 });
 
@@ -36,11 +70,11 @@ transformer.defineResource('menu_items', {
   relationships: [
     {
       resource: 'items.id',
-      field: 'item_id'
+      field: 'item_id' // this is assum,ed to be id if not defined
     },
     {
       resource: 'menus.id',
-      field: 'menu_id'
+      field: 'menu_id' // this is assum,ed to be id if not defined
     }
   ]
 });
@@ -71,15 +105,37 @@ var menuStruct = transformer.defineStruct({
   })
 });
 
-menuStruct.get([], function (error, data) {
-  // console.log(data.data.length);
-  // console.log(data.included.length);
-  // console.log(data.data[0].relationships.menuItems);
-  // console.log(data.included);
-  // console.log(data);
-  console.log('Done');
+var locationStruct = transformer.defineStruct({
+  id: 'locations.id',
+  name: 'locations.name',
+
+  menus: transformer.defineStruct({
+    id: 'menus.id',
+    name: 'menus.name',
+
+    menuItems: transformer.defineStruct({
+      id: 'menu_items.id',
+      name: 'items.name',
+      base_price: 'items.base_price',
+      price: 'menu_items.price'
+    })
+  })
+});
+
+locationStruct.get([41], function () {
+  console.log('done');
   process.exit(0);
 });
+
+// menuStruct.get([], function (error, data) {
+//   // console.log(data.data.length);
+//   // console.log(data.included.length);
+//   // console.log(data.data[0].relationships.menuItems);
+//   // console.log(data.included);
+//   // console.log(data);
+//   console.log('Done');
+//   process.exit(0);
+// });
 
 
 // menuStruct.menuItems.get([], function (error, data) {
